56ef38e4c5929ac25e316d6ebc567a7a48abd966
Vince <75965324+0hAshes@users.noreply.github.com>
2022-09-03 00:18:34 +0000

    edit line 4

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-myName = input("Please enter your name first name, friend: ")
-
-lastName = input("Now, enter your last name. Or else: ")
-
-fullName = myName + " " + lastName 
-
-print("How's it doin'", fullName, "?")
+vivaldiBorn = 1678
+myAge = int(input('What year were you born in? '))
+ageDifference = myAge - vivaldiBorn
+print("Vivaldi is ", ageDifference,"years older than you. Wow you are so young. What is it like?" )
